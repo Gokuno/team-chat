71b723c4bb81f0bdb9f9b4fb710843619ab443a2
Allan Paz <jallanpazg@gmail.com>
2024-09-06 21:57:08 -0700

    agregue registro e incio de session
    agregue funcionalidad a la carta de registro e incio de sesion con usando componentes de shadcn

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { FcGoogle } from "react-icons/fc";
 import { Button } from "@/components/ui/button";
 import {
     Card,
@@ -7,8 +9,17 @@ import {
     CardTitle
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { SignInFlow } from "../types";
 
-export const SignInCard = () => {
+interface SignInCardProps {
+    setState: (setState: SignInFlow) => void;
+};
+
+export const SignInCard = ({ setState }: SignInCardProps) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
         <Card className="w-full h-full p-8">
             <CardHeader className="px-0 pt-0" >
@@ -23,17 +34,17 @@ export const SignInCard = () => {
                 <form className="space-y-2.5">
                     <Input
                         disabled={false}
-                        value=""
-                        onChange={() => { }}
-                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Correo electronico"
                         type="email"
                         required
                     />
                     <Input
                         disabled={false}
-                        value=""
-                        onChange={() => { }}
-                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Contraseña"
                         type="password"
                         required
                     />
@@ -41,6 +52,22 @@ export const SignInCard = () => {
                         Continuar
                     </Button>
                 </form>
+                <Separator />
+                <div className="flex flex-col gap-y-2.5">
+                    <Button
+                        variant="outline"
+                        onClick={() => { }}
+                        size="lg"
+                        disabled={false}
+                        className="w-full relative"
+                    >
+                        <FcGoogle className="size-5 absolute top-3 left-2.5" />
+                        Incia con Google
+                    </Button>
+                </div>
+                <div className="text-xs text-muted-foreground">
+                    ¿No tiene una cuenta aun? <span onClick={() => setState("signUp")} className="text-blue-900 hover:underline cursor-pointer">Registrate</span>
+                </div>
             </CardContent>
         </Card>
     );
--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -1,7 +1,83 @@
-export const SignUpCard = () => {
+import { useState } from "react";
+import { FcGoogle } from "react-icons/fc";
+import { Button } from "@/components/ui/button";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { SignInFlow } from "../types";
+
+interface SignUpCardProps {
+    setState: (setState: SignInFlow) => void;
+};
+
+export const SignUpCard = ({ setState }: SignUpCardProps) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
     return (
-        <div>
-            Sign Up Card
-        </div>
+        <Card className="w-full h-full p-8">
+            <CardHeader className="px-0 pt-0" >
+                <CardTitle>
+                    Registrate para continuar
+                </CardTitle>
+                <CardDescription>
+                    Use su correo electronico u otro servicio para continuar
+                </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-5 px-0 pb-0">
+                <form className="space-y-2.5">
+                    <Input
+                        disabled={false}
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Correo electronico"
+                        type="email"
+                        required
+                    />
+                    <Input
+                        disabled={false}
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Contraseña"
+                        type="password"
+                        required
+                    />
+                    <Input
+                        disabled={false}
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        placeholder="Confirma contraseña"
+                        type="password"
+                        required
+                    />
+                    <Button variant="slack" type="submit" className="w-full" size="lg" disabled={false}>
+                        Continuar
+                    </Button>
+                </form>
+                <Separator />
+                <div className="flex flex-col gap-y-2.5">
+                    <Button
+                        variant="outline"
+                        onClick={() => { }}
+                        size="lg"
+                        disabled={false}
+                        className="w-full relative"
+                    >
+                        <FcGoogle className="size-5 absolute top-3 left-2.5" />
+                        Incia con Google
+                    </Button>
+                </div>
+                <div className="text-xs text-muted-foreground">
+                    ¿Ya tienes una cuenta? <span onClick={() => setState("signIn")} className="text-blue-900 hover:underline cursor-pointer">Inicia sesion</span>
+                </div>
+            </CardContent>
+        </Card>
     );
 };

f681c84637cc36dca1134fabea7195b4b67d3f7b
Allan Paz <jallanpazg@gmail.com>
2024-11-01 01:40:06 -0700

    trabajando vista de fecha de mensaje

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -9,17 +9,18 @@ import { useChannelId } from "@/hooks/use-channel-id";
 
 import { Header } from "./header";
 import { ChatInput } from "./chat-input";
+import { MessageList } from "@/components/message-list";
 
 
 const ChannelIdPage = () => {
     const channelId = useChannelId();
 
-    const { results } = useGetMessages({ channelId });
+    const { results, status, loadMore } = useGetMessages({ channelId });
     const { data: channel, isLoading: channelLoading } = useGetChannel({ id: channelId });
 
     console.log({ results });
 
-    if (channelLoading) {
+    if (channelLoading || status === "LoadingFirstPage") {
         return (
             <div className="h-full flex-1 flex items-center justify-center">
                 <Loader className="animate-spin size-6 text-gray-500" />
@@ -41,9 +42,14 @@ const ChannelIdPage = () => {
     return (
         <div className="flex flex-col h-full">
             <Header title={channel.name} />
-            <div className="flex-1">
-                {JSON.stringify(results)}
-            </div>
+            <MessageList
+                channelName={channel.name}
+                channelCreationTime={channel._creationTime}
+                data={results}
+                loadMore={loadMore}
+                isLoadingMore={status === "LoadingMore"}
+                canLoadMore={status === "CanLoadMore"}
+            />
             <ChatInput placeholder={`Mensaje # ${channel.name}`} />
         </div>
     );
---
+++ src/components/message-list.tsx
@@ -0,0 +1,71 @@
+import { format, isToday, isYesterday } from "date-fns";
+import { es } from 'date-fns/locale/es';
+
+import { GetMessagesReturnType } from "@/features/messages/api/use-get-messages";
+
+interface MessageListProps {
+    memberName?: string;
+    memberImage?: string;
+    channelName?: string;
+    channelCreationTime?: number;
+    variant?: "channel" | "thread" | "conversation";
+    data?: GetMessagesReturnType | undefined;
+    loadMore: () => void;
+    isLoadingMore: boolean;
+    canLoadMore: boolean;
+};
+
+const formatDateLabel = (dateStr: string) => {
+    const locale = es;
+    const date = new Date(dateStr);
+    if (isToday(date)) return "Hoy";
+    if (isYesterday(date)) return "Ayer";
+    return format(date, "EEEE, MMMM d", { locale });
+}
+
+export const MessageList = ({
+    memberName,
+    memberImage,
+    channelName,
+    channelCreationTime,
+    data,
+    variant = "channel",
+    loadMore,
+    isLoadingMore,
+    canLoadMore,
+}: MessageListProps) => {
+    const groupedMessages = data?.reduce(
+        (groups, message) => {
+            const date = new Date(message._creationTime);
+            const dateKey = format(date, "yyyy-MM-dd");
+            if (!groups[dateKey]) {
+                groups[dateKey] = [];
+            }
+            groups[dateKey].unshift(message);
+            return groups;
+        },
+        {} as Record<string, typeof data>
+    );
+
+    return (
+        <div className="flex-1 flex flex-col-reverse pb-4 overflow-y-auto messages-scrollbar">
+            {Object.entries(groupedMessages || {}).map(([dateKey, messages]) => (
+                <div key={dateKey}>
+                    <div className="text-center my-2 relative">
+                        <hr className="absolute top-1/2 left-0 right-0 border-t border-gray-300" />
+                        <span className="relative inline-block bg-white px-4 py-1 rounded-full text-xs border border-gray-300 shadow-sm">
+                            {formatDateLabel(dateKey)}
+                        </span>
+                    </div>
+                    {messages.map((message, index) => {
+                        return (
+                            <div key={message._id}>
+                                {JSON.stringify(message)}
+                            </div>
+                        )
+                    })}
+                </div>
+            ))}
+        </div>
+    );
+};
